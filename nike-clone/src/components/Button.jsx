5a5867445d47b79a56acd5cb892abf420a90a823
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2024-01-07 21:21:13 -0500

    creando clon de landing page de nike - sections(popular products, super quality, services, special offer)

--- nike-clone/src/components/Button.jsx
+++ nike-clone/src/components/Button.jsx
@@ -1,12 +1,20 @@
-export function Button({label, iconUrl}) {
+export function Button({label, iconUrl, styleClass}) {
   return (
-    <button className="flex justify-center items-center px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center items-center px-7 py-4 font-montserrat text-lg leading-none  rounded-full ${
+        styleClass !== undefined
+          ? styleClass
+          : 'text-white border-coral-red bg-coral-red'
+      }`}
+    >
       {label}
-      <img
-        src={iconUrl}
-        alt="icon button"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt="icon button"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 }
--- nike-clone/src/sections/Hero.jsx
+++ nike-clone/src/sections/Hero.jsx
@@ -16,7 +16,7 @@ export default function Hero() {
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer Collections
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
             The New Arrival
           </span>
@@ -30,8 +30,8 @@ export default function Hero() {
         <Button label="Shop now" iconUrl={arrowRight} />
 
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
-          {statistics.map((stat, index) => (
-            <div key={index}>
+          {statistics.map((stat) => (
+            <div key={stat.label}>
               <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
               <p className="leading-7 font-montserrat text-slate-gray">
                 {stat.label}
@@ -50,8 +50,8 @@ export default function Hero() {
           className="object-contain relative z-10"
         />
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
-          {shoes.map((shoe) => (
-            <div key={shoe}>
+          {shoes.map((shoe, index) => (
+            <div key={index}>
               <ShoeCard
                 imageUrl={shoe}
                 changeBigShoe={(shoe) => setBigShoeImg(shoe)}
